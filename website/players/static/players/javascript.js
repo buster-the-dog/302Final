9b2d8ff68bb60439fc87039a557e8143fdb01b67
jmandzak <jmandzak@vols.utk.edu>
2020-04-18 23:14:34 -0400

    Made the table into a dataTable, new functionality

--- website/players/static/players/javascript.js
+++ website/players/static/players/javascript.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    $('#players').DataTable();
-} );
+    $('#players').DataTable( {
+        "ordering": true,
+        "scrollX": true,
+        "scrollY": 500
+    }
+    );
+    $('.dataTables_length').addClass('bs-select');
+});
