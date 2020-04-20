565aeeb60932bca8da5815f3284fe0326285f10d
jmandzak <jmandzak@vols.utk.edu>
2020-04-19 20:53:07 -0400

    Fixed auto sort

--- website/players/static/players/javascript.js
+++ website/players/static/players/javascript.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
     $('#players').DataTable( {
-        "order": [[2, "asc"]],
+        "order": [[4, "asc"]],
         "scrollX": true,
         "scrollY": 500,
     }
     );
     $('.dataTables_length').addClass('bs-select');
 });
-
-$('[data-toggle="tooltip"]').tooltip();
-
